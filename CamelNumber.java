3dada76901bb0c44ed6c7a557081e663fb3f7681
Manvish2000 <61080024+Manvish2000@users.noreply.github.com>
2020-12-03 13:49:38 +0530

    Add files via upload

---
+++ CamelNumber.java
@@ -0,0 +1,31 @@
+package Strings;
+import java.util.*;
+public class CamelNumber 
+{
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("enter String: ");
+		String input = in.next(); 
+		
+		int len = input.length();  
+		
+		if(len == 0)
+		{
+			System.out.println(0);
+		}
+		
+		int count = 1;
+		
+		for(int i =0; i<len; i++)
+		{
+			char c = input.charAt(i);
+			if( 'A' <= c && c <='Z')
+				count++; 
+			else if(c == ' ')
+				break;				
+		}
+		System.out.println(count);
+		
+	}
+}
